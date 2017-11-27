c6850b203e56ad395aa92f5e8675f7c66f9c19d1
s.essadek <s.essadek@pyxicom.com>
2017-11-27 14:44:40 +0000

    first commiot

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+import config from '../../config'
+console.log(config)
+
+// document.getElementById('button').addEventListener("click", function() {
+// 	import('jquery').then(function($) {
+// 		$('body').css("backgroundColor", "#DDD")
+// 	})
+// })
